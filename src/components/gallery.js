0e62d002513a60c2a65a804fc357a03b96655757
Venus Ang <venus@users.noreply.github.com>
2022-10-19 16:47:31 -0700

    added logic to iterate through posting object and render each post

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,10 +1,15 @@
 import Posting from "./posting";
 
 function Gallery() {
+  const craigsListPostings = require('../postings');
+
+  let postList = craigsListPostings.postings.map((data, i) => {
+    return <Posting data={data} key={`post - ${i}`} />
+  });
   return (
     <div className='gallery'>
       <h1>Gallery</h1>
-      <Posting />
+      {postList}
     </div>
   );
 }
--- src/components/posting.js
+++ src/components/posting.js
@@ -1,7 +1,13 @@
-function Posting() {
+function Posting(props) {
+  // let posting = props.data; 
+  // ask if using props.data is a React best practices - seems weird to me
+
   return (
     <div className="posting">
-      <h1>Posting</h1>
+      <div>{props.data.price}</div>
+      <img src={props.data.imageURL} alt={props.data.description} />
+      <h3>{props.data.title}</h3>
+      <p>{props.data.description}</p>
     </div>
   )
 }
